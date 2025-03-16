e9d3196589cbbd10a35cf77cfe9edfade2db8225
María Paz Janicki <mjanicki@udesa.edu.ar>
2025-03-16 04:45:14 -0300

    parches ej2

--- Ej2.cpp
+++ Ej2.cpp
@@ -3,26 +3,30 @@
 
 using namespace std;
 
+
+
 enum class ETIQUETAS : uint8_t {ERROR,DEBUG, INFO, WARNING,CRITICAL, iNVALID};
+string enum_to_string(ETIQUETAS severidad);
+
 void logmessage(string msg, ETIQUETAS severidad){
 
     ofstream outFile("Registro_funcionamiento.txt", ios::app);
     if(outFile.is_open()){
-        outFile <<"[" << static_cast<int>(severidad) << "] <" <<msg <<">\n";
+        outFile <<"[" << enum_to_string(severidad) << "] <" <<msg <<">\n";
     }
 }
 
 void logmessage(string msg, string file, int error_line){
     ofstream outFile("Registro_funcionamiento.txt", ios::app);
     if(outFile.is_open()){
-        outFile <<"[ERROR] <" << msg <<"> Archivo:" << file << ". Linea del error:"<<error_line<<"\n";
+        outFile <<"[ERROR] <" << msg <<"> Archivo:" << file << " Linea del error:"<<error_line<<"\n";
     }
 }
 
 void logmessage(string access_msg, string username){
     ofstream outFile("Registro_funcionamiento.txt", ios::app);
     if(outFile.is_open()){
-        outFile <<"[SEGURIDAD]" <<access_msg <<">\n";
+        outFile <<"[SECURITY]" <<"<"<<username <<">"<< access_msg<<"\n";
     }
 }
 ETIQUETAS make_enum_element(string severidad){
@@ -30,12 +34,12 @@ ETIQUETAS make_enum_element(string severidad){
         if(severidad == "ERROR" || severidad == "error"){
             return ETIQUETAS::ERROR;
         }
-        if(severidad == "DEBUG" || severidad == "ERROR"){
+        if(severidad == "DEBUG" || severidad == "debug"){
             return ETIQUETAS::DEBUG;
         }
-        if(severidad == "INFO" || severidad == "ERROR"){
+        if(severidad == "INFO" || severidad == "info"){
             return ETIQUETAS::INFO;
-        }if(severidad == "WARNING" || severidad == "ERROR"){
+        }if(severidad == "WARNING" || severidad == "warning"){
             return ETIQUETAS::WARNING;
         }if(severidad == "CRITICAL" || severidad == "critical"){
             return ETIQUETAS::CRITICAL;
@@ -51,31 +55,109 @@ ETIQUETAS make_enum_element(string severidad){
     
 }
 
+string enum_to_string(ETIQUETAS severidad){
+    switch(severidad){
+        case ETIQUETAS::ERROR: 
+            return "ERROR";
+        case ETIQUETAS::DEBUG: 
+            return "DEBUG";
+        case ETIQUETAS::INFO: 
+            return "INFO";
+        case ETIQUETAS::WARNING: 
+            return "WARNING";    
+        case ETIQUETAS::CRITICAL: 
+            return "CRITICAL";
+        default:
+            return "INVALID";
+    }
+}
+
 int main(){
-    string severidad;
+    int op;
+    
     string msg;
-    bool valid = true;
-    ETIQUETAS enum_severidad;
-    while(valid){
-        
-        cout << "Ingrese la etiqueta de severidad:" <<" ";
-        cin >> severidad;
-        enum_severidad = make_enum_element(severidad);
-    }
-    cout << "Ingrese el mensaje:" <<" ";
-    cin >> msg;
-    logmessage(msg, enum_severidad);
-
-    try{
-        string a;
-        string b;
-        cin >> a;
-        cin >> b;
-        if(a == b){
-            throw "Numeros distintos";
+    char menu;
+    bool inicio = true;
+    while(inicio){
+        cout << "\nEliga la operación que desea registrar:\n1.Severidades comunes: ERROR,DEBUG,INFO,WARNING,CRITICAL\n2.Otros errores\n3.Acceso de usuario\n4.Prueba funcionalidad\n5.SALIR"<<endl;
+        cin >> op;
+        switch(op){
+            case 1:{
+                string severidad;
+                
+                bool valid = true;
+                ETIQUETAS enum_severidad;
+                while(valid){
+                    cout << "Ingrese la etiqueta de severidad:" <<" ";
+                    cin >> severidad;
+                    enum_severidad = make_enum_element(severidad);
+                    if(enum_severidad != ETIQUETAS::iNVALID){
+                        valid = false;
+                    }
+                }
+                cout << "Ingrese el mensaje:" <<" ";
+                cin >> msg;
+                logmessage(msg, enum_severidad);
+                break;
+            }
+            case 2:{
+                string file;
+                int error_line;
+                cout << "Ingrese el nombre de su error, el nombre del archivo y la linea de código donde se encontraba el error:" << " ";
+                cin >> msg>> file >>error_line;
+                logmessage(msg,file,error_line);
+                break;
+            }
+            case 3:{
+                string username;
+                cout << "Ingrese el usuario y el mensaje de acceso: "<< " ";
+                cin >> username >> msg;
+                logmessage(msg, username);
+                break;
+            }
+            case 4:{
+                try{
+                    string a;
+                    string b;
+                    cout <<"\n COMPARACIÓN DE STRINGS\nIngrese el valor de la 1ra variable: ";
+                    cin >> a;
+                    cout <<"Ingrese el valor de la 2da variable: ";
+                    cin >> b;
+                    if(a != b){
+                        throw runtime_error("Strings distintos!");
+                    }
+                    cout << "Strings iguales!\n";
+                }catch(const runtime_error& e){
+                    logmessage(e.what(), ETIQUETAS::ERROR);
+                    return 1;
+                }
+                break;
+            }
+            case 5:
+                return 0;
+            default:
+                cout << "\nIngreso un comando inválido, intente nuevamente.\n";
+                
         }
-    }catch(const char* e){
-        logmessage(e, ETIQUETAS::ERROR);
-    }
-
+        try{
+            cout << "¿Desea realizar otra operación? Y/N\n";
+            cin >>menu;
+            if(menu == 'N' || menu == 'n'){
+                cout << "Cerrando...";
+                inicio = false;
+            }
+            else if(menu == 'Y' || menu == 'y'){
+                continue;
+            }
+            else{
+                throw "\nComando invalido, escriba 'Y' o 'N'. Vuelva a intentar.\n";
+            }
+        }
+        catch(const char* f){
+            cout << f;
+        }
+            
+    }  
+ 
+    return 0;
 }
